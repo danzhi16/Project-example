05a083dec2800059fe41ef12ac49c707472699e3
aminnrvi <242046@astanait.edu.kz>
2025-02-01 07:27:38 +0500

    database

--- Main.java
+++ Main.java
@@ -14,6 +14,7 @@ class Database {
     private static final String USER = "postgres";
     private static final String PASSWORD = "290607";
 
+
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
