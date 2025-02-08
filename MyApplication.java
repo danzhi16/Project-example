c24ea7f41c145d3c6d560ceb71bbfe448703f5e6
aminnrvi <242046@astanait.edu.kz>
2025-02-08 08:42:50 +0500

    usermenu

--- MyApplication.java
+++ MyApplication.java
@@ -122,6 +122,8 @@ public class MyApplication {
         System.out.println("1. View Profile");
         System.out.println("2. Logout");
         System.out.println("3. List of categories");
+        System.out.println("4. List of products");
+        System.out.println("0. Exit");
 
         try {
             int option = scanner.nextInt();
@@ -134,6 +136,8 @@ public class MyApplication {
                     return;
                 case 3:
                     getAllCategories(); break;
+                case 4:
+                    getAllProducts(); break;
                 default:
                     System.out.println("Invalid option.");
             }
