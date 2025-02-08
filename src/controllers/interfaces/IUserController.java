136ff01a7deef2aa138badda4dbb022792e68c0e
aminnrvi <242046@astanait.edu.kz>
2025-02-08 08:23:46 +0500

    src

--- src/controllers/interfaces/IUserController.java
+++
@@ -1,9 +0,0 @@
-package controllers.interfaces;
-
-public interface IUserController {
-    String createUser(String username, String password, String gender, String role);
-    String getUserById(int id);
-    String getAllUsers();
-    String deleteUser(int id);
-    boolean loginUser(String username, String password);
-}
--- src/service/interfaces/IValidationService.java
+++
@@ -1,6 +0,0 @@
-package service.interfaces;
-
-public interface IValidationService {
-    boolean isValidEmail(String email);
-    boolean isValidPrice(double price);
-}
