f0773ed3a2cd651b163ffef6e3c4300bba1a3786
aminnrvi <242046@astanait.edu.kz>
2025-02-08 08:05:56 +0500

    improved business idea

--- src/service/ProductService.java
+++ src/service/ProductService.java
@@ -1,16 +1,8 @@
 package service;
 
 import controllers.interfaces.ICategoryController;
-import models.Category;
-import models.Product;
 import repositories.CategoryRepository;
 import repositories.ProductRepository;
-import service.interfaces.IConsoleApplication;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class ProductService {
     private final CategoryRepository categoryRepository;
